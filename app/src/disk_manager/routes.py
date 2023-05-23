f199c0d964a3b6a4d3e91d3dd16849c170c02dff
RomaOkorosso <zatrutin.roma.work@gmail.com>
2023-05-23 08:46:58 +0300

    Update routes.py

--- app/src/disk_manager/routes.py
+++ app/src/disk_manager/routes.py
@@ -121,7 +121,7 @@ async def format_disk(
     if platform.system() == "Windows":
         format_cmd = ["format", db_disk.name, "/FS:NTFS", "/Q"]
     else:
-        format_cmd = ["sudo", "mkfs.ext4 -F", f"/dev/{db_disk.name}"]
+        format_cmd = ["sudo", "mkfs.ext4", "-F", f"/dev/{db_disk.name}"]
 
     try:
         disk_service.run_shell_command(format_cmd)
